61e173c3522dda315075f3e4b5eca42eb4390406
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-01-07 17:33:40 +0100

    int[] obtenerPosicionInicio

--- src/IndianaJones.java
+++ src/IndianaJones.java
@@ -1,5 +1,15 @@
 public class IndianaJones {
+    final static int X_INDIANA_JONES = 2;
+    final static int Y_INDIANA_JONES = 2;
     public static void main(String[] args) {
 
     }
+    /**
+     * Obtiene la posición de inicio de Indiana Jones.
+     * Genera una posición transitable por el personaje dentro del escenario.
+     */
+    public static int[] obtenerPosicionInicio(){
+        return new int[]{X_INDIANA_JONES, Y_INDIANA_JONES};
+    }
+
 }
